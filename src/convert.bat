685a8acd3a1cae5dece4a04f8ff3baed491364a4
HoboJoe2 <joedickson4556@gmail.com>
2024-09-13 12:55:01 +1000

    attempt 2..

--- src/convert.bat
+++ src/convert.bat
@@ -2,8 +2,10 @@
 @echo off
 
 :: Run script from the output folder so that the resulting glb file ends up there
-mkdir "\\CAVE-HEADNODE\data\3dvis\models"
-cd "\\CAVE-HEADNODE\data\3dvis\models"
+::mkdir "\\CAVE-HEADNODE\data\3dvis\models"
+::cd "\\CAVE-HEADNODE\data\3dvis\models"
+mkdir "..\models"
+cd "..\models"
 
 :: -b is for background mode, -P means "run python script [script]", -- seperates the args for blender from the args for python, %1 is the arg
 :: intended for 2gltf2.py (which is the argument passed into this script)
