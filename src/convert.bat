69642b98489c1e36f98dd4a51451aaec590ec0a9
HoboJoe2 <joedickson4556@gmail.com>
2024-09-13 12:25:54 +1000

    almost working on laptop

--- src/convert.bat
+++ src/convert.bat
@@ -6,4 +6,4 @@ cd "..\models"
 
 :: -b is for background mode, -P means "run python script [script]", -- seperates the args for blender from the args for python, %1 is the arg
 :: intended for 2gltf2.py (which is the argument passed into this script)
-"..\Blender 4.2\blender.exe" -b -P "..\2gltf2\2gltf2.py" -- %1
+"..\Blender 4.2\blender.exe" -b -P "..\2gltf2_3DVis\2gltf2.py" -- %1
